1c855cf69842294b37b1691d84e9978a919cbd31
sinisa.miskovic@sattrakt.com <sinisa.miskovic@sattrakt.com>
2024-06-29 10:22:48 +0200

    clean code

--- src/pages/userList/UserList.page.tsx
+++ src/pages/userList/UserList.page.tsx
@@ -6,8 +6,6 @@ import { usePlaceholderUsers } from '@homework-task/api/placeholderApi.api';
 const UserListPage = () => {
     const { isLoading, error, data } = usePlaceholderUsers();
 
-    console.log(data);
-
     if (isLoading) return 'Loading users from placeholder api...';
 
     if (error) return 'Error fetching users';
@@ -15,17 +13,27 @@ const UserListPage = () => {
     return (
         <Fragment>
             <h1>User List</h1>
-            <ul
-       
-          >
+            <ul>
                 {data?.map((user) => (
-                    <li key={user.id}
-                  >
-                       <span><b>User id:</b> <i>{user.id}</i></span> {" "}
-                       <span><b>User name: </b><i>{user.name}</i></span> {" "}
-                       <span><b>Username:</b> <i>{user.name}</i></span> {"  "}
-                       <span><b>User email:</b> <i>{user.email}</i></span>{"  "}
-                       <span><b>User phone:</b> <i>{user.phone}</i></span>
+                    <li key={user.id}>
+                        <span>
+                            <b>User id:</b> <i>{user.id}</i>
+                        </span>{' '}
+                        <span>
+                            <b>User name: </b>
+                            <i>{user.name}</i>
+                        </span>{' '}
+                        <span>
+                            <b>Username:</b> <i>{user.name}</i>
+                        </span>{' '}
+                        {'  '}
+                        <span>
+                            <b>User email:</b> <i>{user.email}</i>
+                        </span>
+                        {'  '}
+                        <span>
+                            <b>User phone:</b> <i>{user.phone}</i>
+                        </span>
                     </li>
                 ))}
             </ul>
